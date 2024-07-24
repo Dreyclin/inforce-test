48c96fb0bc55fbd903db28727aef154d1ba65f4b
Maxim Sur <makarka16@gmail.com>
2024-07-24 16:36:29 +0300

    Add: db schemas and models

--- server/app.js
+++ server/app.js
@@ -8,6 +8,28 @@ const app = express();
 
 app.use(cors());
 
+const productSchema = mongoose.Schema({
+    id: String,
+    imgUrl: String,
+    name: String,
+    count: Number,
+    size: {
+        width: Number,
+        height: Number
+    },
+    weight: String,
+    comments: [commentSchema]
+})
+
+const commentSchema = mongoose.Schema({
+    id: String,
+    productId: String,
+    description: String,
+    date: Date
+})
+
+const Product = mongoose.model("Product", productSchema)
+const Comment = mongoose.model("Comment", commentSchema)
 
 app.get("/", (req, res) => {
     res.send("HELLO WORLD!");
